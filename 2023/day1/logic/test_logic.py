a846e277f4aa7bee6f7139481d1e149964363fd2
Martin de Haan <martin@MacBook-Pro-van-Martin.local>
2023-12-01 14:29:03 +0100

    Day1, part1

--- 2023/day1/logic/test_logic.py
+++ 2023/day1/logic/test_logic.py
@@ -1,31 +1,39 @@
 import unittest
-from logic import read_input_file, calculate_floor
+from logic import read_file_lines, extract_numbers, add_all_numbers
 
 
 class TestLogicFunctions(unittest.TestCase):
 
-    def test_read_input_file(self):
-        content = read_input_file('test_input.txt')
-        self.assertEqual(content, '(()(()(')
+    def test_read_read_file_lines(self):
+        content = read_file_lines('test_input.txt')
+        self.assertEqual(content, ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet'])
 
-    def test_count_brackets(self):
-        # Test the count_brackets function
+    def test_read_read_file_error(self):
+        content = read_file_lines('doesNotExist.txt')
+        self.assertEqual(content, [])
+
+    def test_extract_digits(self):
         test_cases = [
-            ("()()", 0),
-            ("(())", 0),
-            ("(((", 3),
-            ("(()(()(", 3),
-            ("))(((((", 3),
-            ("())", -1),
-            ("))(", -1),
-            (")))", -3),
-            (")())())", -3),
+            "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"
+        ]
+        expected_results = [
+            12, 38, 15, 77
         ]
 
-        for input_string, expected_output in test_cases:
-            result = calculate_floor(input_string)
-            print(f"Test result: {result}")
-            self.assertEqual(result, expected_output)
+        results = extract_numbers(test_cases)
+        print(f"\nTest result: {results}")
+        self.assertEqual(results, expected_results)
+
+    def test_add_all_numbers(self):
+        test_cases = [
+            12, 38, 15, 77
+        ]
+        expected_total = 142
+        print(f"\nExpected result = {expected_total}")
+
+        result = add_all_numbers(test_cases)
+        print(f"Test result: {result}")
+        self.assertEqual(result, expected_total)
 
 
 if __name__ == '__main__':
