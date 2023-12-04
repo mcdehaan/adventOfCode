53c89f7e13f3ba8caba6b835a05e5d35544dc441
Martin de Haan <martin@MacBook-Pro-van-Martin.local>
2023-12-04 17:27:48 +0100

    cleanup

--- 2023/day3/main.py
+++ 2023/day3/main.py
@@ -1,8 +1,14 @@
 from file_reader.file_reader import read_file_to_grid
+from grid_interpreter.grid_interpreter import find_symbols, find_full_numbers
+from logic.logic import check_adjacency_for_numbers_with_coordinates, add_qualified_numbers
 
 # Part 1
 if __name__ == '__main__':
-    final_result = 0
     grid = read_file_to_grid('./input.txt')
-    print(f"\nFinal result part 1 = {final_result}")
+    symbol_list = find_symbols(grid)
+    full_number_list = find_full_numbers(grid)
+    adjacent_numbers = check_adjacency_for_numbers_with_coordinates(full_number_list, symbol_list)
+    final_result = add_qualified_numbers(adjacent_numbers)
+    print(f"\nFinal result: {final_result}")
+
 
